e1064888ba86c354f5fb9cf50342e217252f12da
arup rakshit <aruprakshit@rocketmail.com>
2020-01-02 00:34:08 +0530

    Fix warnings

--- react-spa/src/components/Posts/RatePost.js
+++ react-spa/src/components/Posts/RatePost.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useCallback } from 'react';
 import { Grid, Box, Icon } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { green, red } from '@material-ui/core/colors';
@@ -43,26 +43,30 @@ export default function RatePost({ postId, ratings }) {
   const downVote = category => e => {
     addRatingToPost({ variables: { postId, category } });
   };
-  const ratingIds = ratings.map(rating => rating.id);
 
-  const computeVoteCount = category => {
-    if (category === 'UPVOTE') {
-      return ratings
-        .filter(rating => rating.category === category)
-        .reduce((memo, rating) => memo + rating.weight, 0);
-    }
+  const computeVoteCount = useCallback(
+    category => {
+      if (category === 'UPVOTE') {
+        return ratings
+          .filter(rating => rating.category === category)
+          .reduce((memo, rating) => memo + rating.weight, 0);
+      }
 
-    if (category === 'DOWNVOTE') {
-      return ratings
-        .filter(rating => rating.category === category)
-        .reduce((memo, rating) => memo + Math.abs(rating.weight), 0);
-    }
-  };
+      if (category === 'DOWNVOTE') {
+        return ratings
+          .filter(rating => rating.category === category)
+          .reduce((memo, rating) => memo + Math.abs(rating.weight), 0);
+      }
+    },
+    [ratings]
+  );
 
-  const upVoteCount = useMemo(() => computeVoteCount('UPVOTE'), [...ratingIds]);
+  const upVoteCount = useMemo(() => computeVoteCount('UPVOTE'), [
+    computeVoteCount,
+  ]);
 
   const downVoteCount = useMemo(() => computeVoteCount('DOWNVOTE'), [
-    ...ratingIds,
+    computeVoteCount,
   ]);
 
   return (
